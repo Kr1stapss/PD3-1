da80251a3e7ab4df1d72fefd9c734be6acd4b944
Kr1stapss <kristapsb035@gmail.com>
2026-03-16 10:48:50 +0200

    Update 3.ud.ps1

--- 3.ud.ps1
+++ 3.ud.ps1
@@ -1,1 +1,11 @@
+$status = Get-MpComputerStatus
 
+$real = $status.RealTimeProtectionEnabled
+$scan = $status.QuickScanAge
+
+if($real -eq $false -or $scan -gt 3){
+Write-Output "Sistēma ir apdraudēta!"
+}
+else{
+Write-Output "Sistēma ir droša."
+}
