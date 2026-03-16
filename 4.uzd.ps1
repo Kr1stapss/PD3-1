d2927ad6cb301026f9810fb9a4d6289cfd8bf8be
Kr1stapss <kristapsb035@gmail.com>
2026-03-16 11:04:58 +0200

    Update 4.uzd.ps1

--- 4.uzd.ps1
+++ 4.uzd.ps1
@@ -1,1 +1,8 @@
+$source = "$env:USERPROFILE\Documents\Errors.txt"
+$date = Get-Date -Format "yyyy-MM-dd"
+$zip = "$env:USERPROFILE\Documents\Atskaite_$date.zip"
 
+Compress-Archive $source $zip -Force
+
+$size = (Get-Item $zip).Length / 1KB
+Write-Output "Arhīva izmērs: $([math]::Round($size,2)) KB"
